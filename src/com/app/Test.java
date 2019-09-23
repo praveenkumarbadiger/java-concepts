125fa0764fc65fbc4bd65fe056fa41c5690bd018
praveenkumarhopscotch <praveen.kumar.sakha@hopscotch.in>
2019-09-23 17:23:42 +0530

    removing bin folder

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -2,6 +2,10 @@ package com.app;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		String name="genericFilter";
+		String name2="Filter";
+		if(name.contains(name2)) {
+			System.out.println(name.replaceAll(name2, ""));			
+		}
 	}
 }
