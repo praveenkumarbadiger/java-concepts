6a794dcb062b682d039f58c0aa42c0e89274c01c
prave <prave@LAPTOP-E7HJCL4D>
2021-05-22 18:38:36 +0530

    method name changed

--- src/com/serializeAnObject/Test.java
+++ src/com/serializeAnObject/Test.java
@@ -7,11 +7,11 @@ public class Test {
 	public static void main(String[] args) {
 		Person p = new Person("praveen", 20);
 		
-		Optional<String> s = ObjectSerializer.convertToString(p);
+		Optional<String> s = ObjectSerializer.convertObjectToString(p);
 		
 		System.out.println(s);
 		
-		Optional<Person> p1  = ObjectSerializer.convertFrom(s.get());
+		Optional<Person> p1  = ObjectSerializer.convertStringToObject(s.get());
 		
 	}
 
